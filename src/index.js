c454717532cec8e943f3a01d3ac1f960e4595a0e
AB <austinbuhler@gmail.com>
2020-10-06 14:18:43 -0400

    Tasks Added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+
+
+// to grab the input
+const submitButton = document.getElementById('submit-btn')
+// console.log(submitButton)
+
+submitButton.addEventListener('click', function(e) {
+  const taskDescription = document.getElementById('new-task-description')
+  const taskLi = document.createElement('li')
+
+  taskLi.innerHTML = `
+  ${taskDescription.value}
+  <button>&times;</button>
+  `
+  
+  const tasksList = document.querySelector('#tasks')
+  tasksList.append(taskLi)
+
+  e.preventDefault()
+})
+
+const allLis = document.querySelectorAll('li')
+
+const deleteButtons = document.querySelectorAll('button')
+
+for(const button of deleteButtons) {
+  button.addEventListener('click', function(e){
+    const button = e.target
+    const parentLi = button.parentElement
+    console.log(parentLi)
+    
+  })
+}
+
